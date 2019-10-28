07b8321a65ff11e05a58c195f0146c48dfe7521e
Tirta Rachman <tirtawening@gmail.com>
2019-10-28 10:55:35 -0400

    Create blank template for future endeavors

---
+++ template/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+}
+
+function draw() {
+  background(240)
+  text('Make Something', width/2, height/2);
+}
